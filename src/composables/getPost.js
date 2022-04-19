af1a857a116e0b0b23caffc84651863ac3adc8d1
CKMolinga <ck.molinga@gmail.com>
2022-04-19 13:07:28 +0200

    fetching single documents

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,24 +1,27 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPost = (id) => {
 
-  const post = ref(null)
-  const error = ref(null)
+    const post = ref(null)
+    const error = ref(null)
 
-  const load = async () => {
-    try {
-      let data = await fetch('http://localhost:3000/posts/' + id)
-      if (!data.ok) {
-        throw Error('That post does not exist')
-      }
-      post.value =  await data.json()
-    }
-    catch(err) {
-      error.value = err.message
-    }
-  }
+    const load = async() => {
+        try {
+            let res = await projectFirestore.collection('posts').doc(id).get()
 
-  return { post, error, load } 
+            if (!res.exists) {
+                throw Error('Post not found')
+            }
+
+            post.value = {...res.data(), id: res.id }
+            console.log(post.value)
+        } catch (err) {
+            error.value = err.message
+        }
+    }
+
+    return { post, error, load }
 }
 
 export default getPost
